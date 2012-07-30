b1195d03ff14b08c445203420af83a65fee036f1
George Nychis <gnychis@gmail.com>
2012-07-30 01:16:06 -0400

    we do not use an external ubertooth_specan application, so im removing it to not be misleading.  everything is accessed from the ubertooth natively by the android application

--- jni/ubertooth/Android.mk
+++ jni/ubertooth/Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= ubertooth.c ubertooth-specan.c
-LOCAL_MODULE := ubertooth_specan
-LOCAL_C_INCLUDES += jni/libusb jni/libbtbb
-LOCAL_SHARED_LIBRARIES := libc libusb libbtbb
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog 
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= ubertooth.c ubertooth-util.c
 LOCAL_MODULE := ubertooth_util
 LOCAL_C_INCLUDES += jni/libusb jni/libbtbb
